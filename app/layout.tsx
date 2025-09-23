5b8faa8d210900b06731ac34cc037262d63ffba2
Robert Pisani <rob@pisani.ca>
2025-09-22 23:16:51 -0400

    feat: Update title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: 'Hot Club Of Nowhere',
+  description: 'Pre-Bop swing in the hot club format. Mixing Django and hot Jazz favourites.',
 };
 
 export default function RootLayout({
