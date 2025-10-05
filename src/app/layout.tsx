af015a01c4d2a9f2be2205073d206f23aca97264
Robert Pisani <rob@pisani.ca>
2025-10-05 18:24:17 -0400

    feat: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Box from '@mui/material/Box';
 import theme from '@/theme';
 import AppBar from '@/components/AppBar';
 import ModeSwitch from '@/components/ModeSwitch';
+import Copyright from '@/components/Copyright';
 
 export default function RootLayout(props: { children: React.ReactNode }) {
 	return (
@@ -17,10 +18,20 @@ export default function RootLayout(props: { children: React.ReactNode }) {
 				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
 					<ThemeProvider theme={theme}>
 						<CssBaseline />
-						<AppBar title={process.env.NEXT_PUBLIC_WEBSITE_NAME}>
-							<ModeSwitch />
-						</AppBar>
-						{props.children}
+						<Box
+							sx={{
+								display: 'flex',
+								flexDirection: 'column',
+								justifyContent: 'center',
+								alignItems: 'center',
+							}}
+						>
+							<AppBar title={process.env.NEXT_PUBLIC_WEBSITE_NAME}>
+								<ModeSwitch />
+							</AppBar>
+							{props.children}
+							<Copyright />
+						</Box>
 					</ThemeProvider>
 				</AppRouterCacheProvider>
 			</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,10 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Copyright from '@/components/Copyright';
 
 export default function Home() {
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography
-          variant="h3"
-          component="h1"
-          sx={{
-            mb: 2,
-            display: { xs: 'none', sm: 'block' },
-          }}
-        >
-          {process.env.NEXT_PUBLIC_WEBSITE_NAME}
-        </Typography>
-        <Copyright />
-      </Box>
-    </Container>
+    <Box sx={{ display: 'flex' }}>
+      <img src='/images/home.webp' alt='Hot Club Of Nowhere at Steelwheel Brewery' width='100%' />
+    </Box>
   );
 }
